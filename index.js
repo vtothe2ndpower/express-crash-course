5297f3013573be5b64f8d944858678a49fb4cee7
Victor Vargas <vvargasnunez@gmail.com>
2021-01-01 23:44:19 -0800

    Initial version of application

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
 // Init Middleware
 // app.use(logger);
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 // Body Parser Middleware
 app.use(express.json());
 // Handle Url Encoded Data
 app.use(express.urlencoded({ extended: false }))
 
+// Homepage Route
+app.get('/', (req, res) => res.render('index', { 
+  title: 'Member App',
+  members
+ }));
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
